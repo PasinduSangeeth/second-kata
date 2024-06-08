498f0f32b0e85cbc74472002fe6f3e9c54c04d46
Pasindu <pasindusangeeth968@gmail.com>
2024-06-09 00:49:26 +0530

    completed second kata

---
+++ secondkata.js
@@ -0,0 +1,10 @@
+const peoples = [
+    { name: 'malika', age: 25 },
+    { name: 'chalana', age: 30 },
+    { name: 'eran', age: 35 },
+    { name: 'chathurika', age: 35 },
+]
+const peopleNames=peoples.map((people)=>{
+    return people.name
+ })
+console.log(peopleNames); 
